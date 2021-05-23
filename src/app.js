31de3692a5e14ced6be27b34614787dd9f608e1e
coderts23 <tanyasamyal123@gmail.com>
2021-05-23 22:56:15 +0530

    Deploy-2

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs')
 app.set('views', viewsPath)
 
 app.use(session({
-    secret: process.env.SECRET,
+    secret: Thisisourlittlesecret,
     resave: false,
     saveUninitialized: false
 }))
